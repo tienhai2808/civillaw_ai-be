0aab4c89323d2a62bee550fc9b1ab4e1653322de
Hải Tân <221124022310@due.udn.vn>
2025-04-11 19:43:28 +0700

    Hai commit v4

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from api.routes import router
 
-app = FastAPI(title="Legal QA API", description="API trả lời câu hỏi luật dân sự")
+app = FastAPI(title="Civil Law API", description="API trả lời câu hỏi luật dân sự")
 
 origins = [
     "http://localhost:3000",  
@@ -10,10 +10,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # Cho phép các origin này
+    allow_origins=origins,  
     allow_credentials=True,
-    allow_methods=["*"],    # Cho phép tất cả các phương thức (GET, POST, OPTIONS, v.v.)
-    allow_headers=["*"],    # Cho phép tất cả các header
+    allow_methods=["*"],    
+    allow_headers=["*"],   
 )
 
 app.include_router(router)
